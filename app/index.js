b17e4de7391f4334fa712a4b5fdbe43e0562794d
sjoseph1848 <sjoseph1848@gmail.com>
2020-03-15 19:43:35 -0500

    completed initial setup of static html

--- app/index.js
+++ app/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App.js';
-import Popular from './components/Popular'
 
 class App extends React.Component {
     render(){
         return(
-            <div className='container'>
-               <Popular />
-            </div>
+            ""
         )
     }
 }
@@ -21,27 +18,3 @@ ReactDOM.render(
     // Where to render to the element to 
     document.getElementById('app')
 )
-
-
-
-// function isAuth() {
-//     return true
-// }
-
-// function showWarning() {
-//     return true
-// }
-// Component 
-// State 
-// Lifecycle 
-// UI
-
-// Violation of separation of concerns 
-// How you define a component
-// class App extends React.Component {
-//     render() {
-//       return(
-//           <App />
-//       )
-//     }
-// }
--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,11 @@ module.exports = {
 module: {
     rules: [
         { test: /\.(js)$/,use:'babel-loader' },
-        { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+        { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},       
+        {test: /\.(png|svg|jpg|gif)$/, use:['file-loader']}
     ]
 },
+
 mode: 'development',
 plugins: [
     new HtmlWebpackPlugin({
